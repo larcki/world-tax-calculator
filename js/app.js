17c72a4510f603a5e063b6bdc8b224efd123eabe
larcki <lassi.kapraali@gmail.com>
2016-10-28 00:17:28 +0100

    Breakdown component, styling

--- js/app.js
+++ js/app.js
@@ -43,7 +43,7 @@ $(document).ready(function () {
     });
 
 
-    $("#month").click(function() {
+    $("#month").click(function () {
         $('#disabled').removeAttr('disabled');
         $('#year_amount').prop('disabled', true);
     });
@@ -54,32 +54,15 @@ $(document).ready(function () {
 
 });
 
-Vue.component("salary-input", {
-    template: '<div><div class="row"> ' +
-    '<div class="col s4"> <a onclick="swapem()" class="waves-effect red lighten-2 btn">Per month?</a> </div> </div> ' +
-    '<div id="year" class="row ani"> <div class="input-field col s12 m6 l6"> ' +
-        '<input v-model="year_input" id="year_amount" type="number" class="validate" step=1000> ' +
-        '<label id=year_amount_label for="year_amount">Per year</label> ' +
-    '</div> </div> ' +
-    '<div id="month" class="row ani"> <div class="input-field col s12 m6 l6"> ' +
-        '<input disabled v-model="year_input_monthly" id="disabled" type="text" class="validate"> ' +
-        '<label for="disabled">Per Month</label> ' +
-    '</div> </div></div>',
-    props: {
-        data: Object
-    },
-    computed: {
-        countryComponent: function () {
-            return CountryComponents.get(this.data.country)
-        }
-    }
-});
-
 Vue.component("result-item", {
     template: '<li>' +
-    '<div class="collapsible-header">{{data.country}} {{data.yearlyAmount}} {{data.summary.net_year}} {{data.summary.net_month}}</div>' +
+    '<div class="collapsible-header">' +
+    '<div class="col s3">{{data.country}}</div>' +
+    '<div class="col s3">{{data.yearlyAmount}}</div>' +
+    '<div class="col s3">{{data.summary.net_year}}</div>' +
+    '<div class="col s3">{{data.summary.net_month}}</div>' +
+    '</div>' +
     '<div class="collapsible-body">' +
-    '<p>Common stuff for all controls</p>' +
     '<component v-bind:is="countryComponent"></component>' +
     '</div>' +
     '</li>',
@@ -90,6 +73,18 @@ Vue.component("result-item", {
         countryComponent: function () {
             return CountryComponents.get(this.data.country)
         }
+    }
+});
+
+
+Vue.component("breakdown-row", {
+    template: '<div class="breakdown-row">' +
+    '<div class="col s6" >{{title}}</div>' +
+    '<div class="col s4 offset-s2" >{{value}}</div>' +
+    '</div>',
+    props: {
+        title: String,
+        value: Number
     }
 });
 
@@ -127,15 +122,34 @@ function Calculator(country, instance, settings) {
 let UKCalculator = new Calculator('UK', UK);
 let NLCalculator = new Calculator('NL', NL, {
     ruling30: true,
-    holidayAllowance: true
+    holidayAllowance: false
 });
 
 let NLComponent = Vue.extend({
-    template: '<div><input type="checkbox" id="test5" v-model="settings.ruling30"/> <label for="test5">30% Ruling</label></div>',
+    template: '<div class="row" style="padding:20px">' +
+    '<div class="col s6">' +
+        '<breakdown-row title="Taxable Income" :value="breakdown.taxableYear" ></breakdown-row>' +
+        '<breakdown-row title="Income Tax" :value="breakdown.incomeTax" ></breakdown-row>' +
+        '<breakdown-row title="General Tax Credit" :value="breakdown.generalCredit" ></breakdown-row>' +
+        '<breakdown-row title="Labour Tax Credit" :value="breakdown.labourCredit" ></breakdown-row>' +
+        '<breakdown-row title="Net Income" :value="breakdown.netYear" ></breakdown-row>' +
+    '</div>' +
+    '<div class="input-field col s6">' +
+    '<div class="row">' +
+        '<input type="checkbox" class="filled-in" id="test5" v-model="settings.ruling30"/>' +
+        '<label for="test5">30% Ruling</label>' +
+    '</div>' +
+    '<div class="row">' +
+        '<input type="checkbox" class="filled-in" id="test6" v-model="settings.holidayAllowance"/>' +
+        '<label for="test6">Holiday Allowance</label>' +
+    '</div>' +
+    '</div>' +
+    '</div>',
     data: function () {
         return NLCalculator;
     }
 });
+
 
 let UKComponent = Vue.extend({
     template: '<p> this is UK specific component </p>',
