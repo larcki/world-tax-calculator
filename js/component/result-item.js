72cae26edf00487f86863bf0b70d0ebac0a4c9e7
larcki <lassi.kapraali@gmail.com>
2016-11-26 17:57:13 +0000

    Added total tax rate to result titles

--- js/component/result-item.js
+++ js/component/result-item.js
@@ -5,9 +5,9 @@ module.exports = Vue.component("result-item", {
     template: '<li>' +
     '<div class="collapsible-header row no-margin animated fadeIn"  v-bind:class="{ active: data.isExpanded }">' +
     '<div class="col s3 m3 l3 icon" v-bind:class="styleClass">{{data.country}}</div>' +
-    '<div class="col s3">{{data.yearlyAmount}}</div>' +
     '<div class="col s3">{{data.breakdown.netYear}}</div>' +
     '<div class="col s3">{{data.breakdown.netMonth}}</div>' +
+    '<div class="col s3">{{data.breakdown.totalDeductionRate}}</div>' +
     '</div>' +
     '<div class="collapsible-body row no-margin">' +
     '<component v-bind:is="countryComponent"></component>' +
